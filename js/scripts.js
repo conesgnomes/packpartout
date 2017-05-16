e17e1b080b828acd3d37842829ebbde5064754c9
Connor Larsen and Leah Sherrell <connor@>
2017-05-16 16:21:28 -0700

    core prototype returns all parts of needed array

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,16 @@ function Trip(destination, bag, addList) {
   this.addList = [];
 }
 
-function Weather(temperature, humidity) {
-  this.temperature = temperature;
+function Weather(humidity) {
+  this.temperature = [];
   this.humidity = humidity;
 }
 
 
 Weather.prototype.core = function() {
-  // alert("hi");
+  debugger;
+  this.temperature.push("cold");
+  this.temperature.push("hot");
   var masterList = [["Underwear", "Tanktops/T-Shirts"], // every list 0
   ["Shorts", "Light Cotton Pants/Skirts", "Thin Socks", "Sandals", "Brimmed Hat"], // 1 hot and temperate
   ["Light Jacket"], // temperate 2
@@ -23,53 +25,44 @@ Weather.prototype.core = function() {
   ["Thermal Shirts and Pants", "Sweaters", "Thick Socks", "Weatherproof Boots", "Heavy Coat", "Gloves",  "Scarfs", "Winter Hat"]]; //cold 4
 
   var tripList = [masterList[0]];
-  var temp = this.temperature;
-  for (i = 0; i <= this.temperature.length; i++) {
+  for (i = 0; i < this.temperature.length; i++) {
 
-    console.log(temp);
-    if (temp[i] === "hot") {
+
+    if (this.temperature[i] === "hot") {
       tripList.push(masterList[1]);
     }
 
-    if (temp[i] === "temperate") {
-      tripList.push(masterList[1], masterList[2], masterList[3])
+    if (this.temperature[i] === "temperate") {
+      tripList.push(masterList[1], masterList[2], masterList[3]);
     }
-    if (temp[i] === "cold") {
+
+    if (this.temperature[i] === "cold") {
       tripList.push(masterList[3], masterList[4]);
     }
 
-    return tripList;
   }
-  // if (this.temperature === "hot") {
-  //   result = masterList[0] + masterList[1];
-  // } else if (this.temperature === "temperate" || (this.temperature === "hot" && this.temperature === "temperate")) {
-  //   result = masterList[0] + masterList[1] + masterList[2];
-  // } else if (this.temperature === "cold") {
-  //   result = masterList[0] + masterList[3];
-  // } else if () {
-  //
-  // }
+  return tripList;
 
 }
 
 // ui logic
 
-$(function() {
-  var destination;
-  var hot = [];
-  var temperate = [];
-  var cold = [];
-  var bag;
-  var addList = [];
-
-  var newTrip = new Trip (destination);
-
-  $("button#start-quiz").click(function(event) {
-    event.preventDefault();
-    destination = $("#destination").val();
-
-    $(".result").text(destination);
-
-  });
-
-});
+// $(function() {
+//   var destination;
+//   var hot = [];
+//   var temperate = [];
+//   var cold = [];
+//   var bag;
+//   var addList = [];
+//
+//   var newTrip = new Trip (destination);
+//
+//   $("button#start-quiz").click(function(event) {
+//     event.preventDefault();
+//     destination = $("#destination").val();
+//
+//     $(".result").text(destination);
+//
+//   });
+//
+// });
