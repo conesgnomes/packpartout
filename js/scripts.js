02bb11bc30ecf72506c74a6dcb781f8e627a2de6
Connor Larsen and Leah Sherrell <connor@>
2017-05-16 09:56:16 -0700

    add destination input functionality to JS and wrapped form around entire input area in HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,40 @@
 // back-end logic
 //
-function Trip(weather, bag,) {
-  this.weather = Weather();
-
+function Trip(destination, weather, bag, addList) {
+  this.destination = destination;
+  this.weather = weather;
+  this.bag = bag;
+  this.coreList = [];
+  this.addList = [];
 }
 
-function Weather(temperature, climate)
+function Weather(temperature, humidity) {
+  this.temperature = temperature;
+  this.humidity = humidity;
+}
+
+// Trip.prototype.core = function(weather) {
+//   if (w)
+// }
+
+// ui logic
+
+$(function() {
+  var destination;
+  var hot = [];
+  var temperate = [];
+  var cold = [];
+  var bag;
+  var addList = [];
+
+  var newTrip = new Trip (destination);
+
+  $("button#start-quiz").click(function(event) {
+    event.preventDefault();
+    destination = $("#destination").val();
+
+    $(".result").text(destination);
+
+  });
+
+});
