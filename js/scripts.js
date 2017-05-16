abde6cbb8165948e1669a2bf85881b4fa8384848
Connor Larsen and Leah Sherrell <leah@>
2017-05-16 16:48:05 -0700

    create all branches for .core

--- js/scripts.js
+++ js/scripts.js
@@ -8,35 +8,31 @@ function Trip(destination, bag, addList) {
   this.addList = [];
 }
 
-function Weather(humidity) {
-  this.temperature = [];
+function Weather(temperature, humidity) {
+  this.temperature = temperature;
   this.humidity = humidity;
 }
 
 
 Weather.prototype.core = function() {
-  debugger;
-  this.temperature.push("cold");
-  this.temperature.push("hot");
   var masterList = [["Underwear", "Tanktops/T-Shirts"], // every list 0
   ["Shorts", "Light Cotton Pants/Skirts", "Thin Socks", "Sandals", "Brimmed Hat"], // 1 hot and temperate
   ["Light Jacket"], // temperate 2
   ["Comfortable Walking Shoes", "Jeans", "Long Sleeve Shirts"], // temperate and cold 3
   ["Thermal Shirts and Pants", "Sweaters", "Thick Socks", "Weatherproof Boots", "Heavy Coat", "Gloves",  "Scarfs", "Winter Hat"]]; //cold 4
-
   var tripList = [masterList[0]];
   for (i = 0; i < this.temperature.length; i++) {
-
-
-    if (this.temperature[i] === "hot") {
-      tripList.push(masterList[1]);
-    }
-
-    if (this.temperature[i] === "temperate") {
+    if (this.temperature[i] === "hot" && this.temperature[i] === "temperate" && this.temperature[i] === "cold" || (this.temperature[i] === "temperate" && this.temperature[i] === "cold")) {
+      tripList.push(masterList);
+    } else if (this.temperature[i] === "hot" && this.temperature[i] === "temperate") {
       tripList.push(masterList[1], masterList[2], masterList[3]);
-    }
-
-    if (this.temperature[i] === "cold") {
+    } else if (this.temperature[i] === "hot" && this.temperature[i] === "cold") {
+      tripList.push(masterList[1], masterList[3], masterList[4]);
+    } else if (this.temperature[i] === "hot") {
+      tripList.push(masterList[1]);
+    } else if (this.temperature[i] === "temperate") {
+      tripList.push(masterList[1], masterList[2], masterList[3]);
+    } else if (this.temperature[i] === "cold") {
       tripList.push(masterList[3], masterList[4]);
     }
 
