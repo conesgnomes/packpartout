d1116783c70635f7cde19476e510beaddd9da0e9
Leah Sherrell <leahsherrell@icloud.com>
2017-05-16 21:17:48 -0700

    create specific lists in .core prototype using indexOf, use .keyup for destination checkbox

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 // back-end logic
-//
+
 function Trip(destination, bag, addList) {
   this.destination = destination;
   this.weather = {};
@@ -9,56 +9,69 @@ function Trip(destination, bag, addList) {
 }
 
 function Weather(temperature, humidity) {
-  this.temperature = temperature;
-  this.humidity = humidity;
+this.temperature = [];
+  this.humidity = [];
 }
 
-
+// method to build core packing list based on temperature
 Weather.prototype.core = function() {
   var masterList = [["Underwear", "Tanktops/T-Shirts"], // every list 0
-  ["Shorts", "Light Cotton Pants/Skirts", "Thin Socks", "Sandals", "Brimmed Hat"], // 1 hot and temperate
+  ["Shorts", "Light Pants/Skirts", "Thin Socks", "Sandals", "Brimmed Hat"], // 1 hot and temperate
   ["Light Jacket"], // temperate 2
   ["Comfortable Walking Shoes", "Jeans", "Long Sleeve Shirts"], // temperate and cold 3
   ["Thermal Shirts and Pants", "Sweaters", "Thick Socks", "Weatherproof Boots", "Heavy Coat", "Gloves",  "Scarfs", "Winter Hat"]]; //cold 4
   var tripList = [masterList[0]];
   for (i = 0; i < this.temperature.length; i++) {
-    if (this.temperature[i] === "hot" && this.temperature[i] === "temperate" && this.temperature[i] === "cold" || (this.temperature[i] === "temperate" && this.temperature[i] === "cold")) {
-      tripList.push(masterList);
-    } else if (this.temperature[i] === "hot" && this.temperature[i] === "temperate") {
-      tripList.push(masterList[1], masterList[2], masterList[3]);
-    } else if (this.temperature[i] === "hot" && this.temperature[i] === "cold") {
-      tripList.push(masterList[1], masterList[3], masterList[4]);
-    } else if (this.temperature[i] === "hot") {
-      tripList.push(masterList[1]);
-    } else if (this.temperature[i] === "temperate") {
-      tripList.push(masterList[1], masterList[2], masterList[3]);
-    } else if (this.temperature[i] === "cold") {
-      tripList.push(masterList[3], masterList[4]);
-    }
+    if (this.temperature[i] === "hot") {
+        if(tripList.indexOf(masterList[1]) === -1) {
+          tripList.push(masterList[1]);
+        }
+      }
 
+      if (this.temperature[i] === "temperate") {
+        if(tripList.indexOf(masterList[1]) === -1) {
+          tripList.push(masterList[1]);
+        }
+        if(tripList.indexOf(masterList[3]) === -1) {
+          tripList.push(masterList[3]);
+        }
+        tripList.push(masterList[2]);
+      }
+
+      if (this.temperature[i] === "cold") {
+        if(tripList.indexOf(masterList[3]) === -1) {
+          tripList.push(masterList[3]);
+        }
+        tripList.push(masterList[4]);
+      }
   }
   return tripList;
-
 }
 
-// ui logic
+// weather prototype to add to/take away from, core packing list based on humidity
 
-// $(function() {
-//   var destination;
-//   var hot = [];
-//   var temperate = [];
-//   var cold = [];
-//   var bag;
-//   var addList = [];
-//
-//   var newTrip = new Trip (destination);
-//
-//   $("button#start-quiz").click(function(event) {
-//     event.preventDefault();
-//     destination = $("#destination").val();
-//
-//     $(".result").text(destination);
-//
-//   });
-//
-// });
+//trip prototype that changes numbers of items based on size of bag
+
+// UI to gather inputs from form
+
+// UI logic
+
+$(function() {
+  var destination = $("#destination").val();
+  var temperature = [];
+  var humidity = [];
+  // var bag;
+  // var addList = [];
+
+  var newTrip = new Trip(destination);
+  var newWeather = new Weather(temperature, humidity); 
+
+  $("#destination").keyup(function() {
+
+    destination = $(this).val();
+
+    $(".result").html("<h2>" + destination + "</h2>");
+
+  });
+
+});
